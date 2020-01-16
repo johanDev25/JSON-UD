36b3e793c08c95c7399c6632b38995ef673e361d
Johan <johan225@hotmail.es>
2020-01-16 11:53:52 -0500

    Update and destroy products

--- app/controllers/api/v1/products_controller.rb
+++ app/controllers/api/v1/products_controller.rb
@@ -16,7 +16,7 @@ class Api::V1::ProductsController < ApplicationController
 
   def update
   @product = Product.find(params[:id])
-  if @product.update(@product_params)
+  if @product.update(product_params)
     render json: @product, status: 200
   else
     render json: { errors: @product.errors }, status: 422
